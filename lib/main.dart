6a424341c8e4e1a1e979d6943a83e80b5f46fdf4
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-05-15 19:48:48 -0300

    Presentation card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,96 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+                Text(
+                  "Facundo Cristaldo",
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 36,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "FLUTTER DEVELOPER",
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 24,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(
+                    color: Colors.teal[100],
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  //padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 25,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "+598 91 505 540",
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  // padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 25,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "myemail@gmail.com",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
